af662a6d69a9216bff0151cc316b1eeaf216ac11
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-06-14 08:53:35 +0200

    added property entity

---
+++ HotelOpt.Domain/Entities/Property.cs
@@ -0,0 +1,35 @@
+﻿using HotelOpt.Domain.Common;
+
+namespace HotelOpt.Domain.Entities;
+
+public class Property:BaseEntity
+{
+    public string Name { get; private set; }
+    public string ContactEmail { get; private set; }
+    public string PhoneNumber { get; private set; }
+    public string Address { get; private set; }
+    public int StarRating { get; private set; }
+    public Guid TenantId { get; private set; }
+    public Tenant Tenant { get; private set; }
+
+    private Property()
+    {
+        Name = null!;
+        ContactEmail = null!;
+        PhoneNumber = null!;
+        Address = null!;
+        StarRating = 1;
+        Tenant = null!;
+    }
+
+    public Property(string name, string contactEmail, string phoneNumber, int starRating, string address, Guid tenantId)
+    {
+        Name = name;
+        ContactEmail = contactEmail;
+        PhoneNumber = phoneNumber;
+        StarRating = starRating;
+        Address = address;
+        TenantId = tenantId;
+    }
+    
+}
