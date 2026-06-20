f464674a627729cd1590d9be9049aea1a39403d8
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-06-20 10:18:11 +0200

    added validation for registration form

---
+++ HotelOpt.Application/Validators/RegistrationDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using FluentValidation;
+using FluentValidation.Validators;
+using HoteOpt.Application.DTOs;
+
+namespace HoteOpt.Application.Validators;
+
+public class RegistrationDtoValidator:AbstractValidator<RegistrationDto>
+{
+    public RegistrationDtoValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().MaximumLength(20).Matches("^[^0-9]*$");
+        RuleFor(x => x.Surname).NotEmpty().MaximumLength(20).Matches("^[^0-9]*$");
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.Password).NotEmpty().MinimumLength(8).Matches("[A-Z]").Matches("[0-9]")
+            .Matches("[!@#$%^&*(),.?\":{}|<>]");
+    }
+}
