8b5c4029799181f1475271febe9576a29e948c6e
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-07-09 11:15:13 +0200

    added billing service by using stripe

---
+++ HotelOpt.Api/Controllers/WebHookController.cs
@@ -0,0 +1,49 @@
+﻿using HotelOpt.Application.Interfaces;
+using HotelOpt.Domain.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelOpt.Controllers;
+[ApiController]
+[Route("api/subscription")]
+public class WebHookController:ControllerBase
+{
+    private ISubscriptionService _service;
+    private ICurrentTenantService _tenantService;
+    private IConfiguration _configuration;
+    public WebHookController(ISubscriptionService service, ICurrentTenantService tenantService, IConfiguration configuration)
+    {
+        _service = service;
+        _tenantService = tenantService;
+        _configuration = configuration;
+    }
+    [Authorize]
+    [HttpPost]
+    [Route("subscribe")]
+    public async Task<IActionResult> Subscribe([FromBody] SubscriptionPlan plan, string priceId)
+    {
+        await _service.SubscribeAsync(_tenantService.TenantId,plan, priceId);
+        return Ok(new {message="Subscription is updated"});
+    }
+    
+    
+    [HttpPost]
+    [Route("webhook")]
+    public async Task<IActionResult> WebHook()
+    {
+        try
+        {
+            using var reader = new StreamReader(HttpContext.Request.Body);
+            string json = await reader.ReadToEndAsync();
+            string signature = Request.Headers["Stripe-Signature"]!;
+            string webhookSecret = _configuration["Stripe:WebhookSecret"]!;
+            await _service.HandleWebhookAsync(json, signature, webhookSecret);
+            return Ok(new { message = "WebHook is verified" });
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"WEBHOOK ERROR: {ex.Message}");
+            return StatusCode(500, ex.Message);
+        }
+    }
+}
--- HotelOpt.Api/obj/Debug/net10.0/HotelOpt.Api.AssemblyInfo.cs
+++ HotelOpt.Api/obj/Debug/net10.0/HotelOpt.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelOpt.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0753941f6b8a85314852978011494fb935be4eb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+327968b6fc58bd5e719d34fb3d6b9bf2e7392db0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelOpt.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelOpt.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
