927c0bcd53d97993e7d7a15e312454bcafbb5c77
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-07-23 22:51:34 +0200

    added user object to the response

--- HotelOpt.Api/obj/Debug/net10.0/HotelOpt.Api.AssemblyInfo.cs
+++ HotelOpt.Api/obj/Debug/net10.0/HotelOpt.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelOpt.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+221c0bcd0817eb4aca824eb265da942c163cc6f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39a55ea61cba5f7f6eabf64e0428d6e29452309f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelOpt.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelOpt.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
