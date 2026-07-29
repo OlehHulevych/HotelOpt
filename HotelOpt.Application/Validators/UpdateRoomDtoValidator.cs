d1b5e29119b24e5a258d1a4e06587cdea3a11b49
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-07-29 08:56:25 +0200

    updated validator for updating room

---
+++ HotelOpt.Application/Validators/UpdateRoomDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using HotelOpt.Application.DTOs;
+
+namespace HotelOpt.Application.Validators;
+
+public class UpdateRoomDtoValidator:AbstractValidator<UpdateRoomDto>
+{
+    public UpdateRoomDtoValidator()
+    {
+        RuleFor(x => x.RoomNumber).MaximumLength(10).When(x => x.RoomNumber != null);
+        RuleFor(x => x.Description).MaximumLength(500).When(x=>x.Description != null);
+        RuleFor(x => x.Type).IsInEnum().When(x => x.Type != null);
+        RuleFor(x => x.PropertyId).NotEmpty().When(x => x.PropertyId != null);
+    }
+}
--- HotelOpt.Infrastructure/obj/Debug/net10.0/HotelOpt.Infrastructure.AssemblyInfo.cs
+++ HotelOpt.Infrastructure/obj/Debug/net10.0/HotelOpt.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelOpt.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68f5585b082230378603f2cd11fc5a1fcbbaf3b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baf876a8fd2accc57a9ba9d25ecaa02db41ff7c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelOpt.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelOpt.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
