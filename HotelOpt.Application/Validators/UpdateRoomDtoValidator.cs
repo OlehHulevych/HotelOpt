77ec64933799501f90421e63aa1a8ac74ab86674
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-07-29 09:38:26 +0200

    added validation for price per night

--- HotelOpt.Application/Validators/UpdateRoomDtoValidator.cs
+++ HotelOpt.Application/Validators/UpdateRoomDtoValidator.cs
@@ -7,9 +7,10 @@ public class UpdateRoomDtoValidator:AbstractValidator<UpdateRoomDto>
 {
     public UpdateRoomDtoValidator()
     {
-        RuleFor(x => x.RoomNumber).MaximumLength(10).When(x => x.RoomNumber != null);
+        RuleFor(x => x.RoomNumber).MaximumLength(20).When(x => x.RoomNumber != null);
         RuleFor(x => x.Description).MaximumLength(500).When(x=>x.Description != null);
         RuleFor(x => x.Type).IsInEnum().When(x => x.Type != null);
         RuleFor(x => x.PropertyId).NotEmpty().When(x => x.PropertyId != null);
+        RuleFor(x => x.PricePerNight).GreaterThan(0).When(x => x.PricePerNight != null);
     }
 }
